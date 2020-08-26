f9ead4066ac2c79f2ba1ba79794c3515c11b0c8e
sidiDev <sididev3@gmail.com>
2020-08-26 08:33:07 +0000

    Updates

---
+++ precache-manifest.25378e7a89907b43aa3b854a5da12b8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdef5ddaad6eb310b74963b51e87261c",
+    "url": "/react_portfolio_app/index.html"
+  },
+  {
+    "revision": "cd17a0ef35fe3684a5ae",
+    "url": "/react_portfolio_app/static/css/main.2284032d.chunk.css"
+  },
+  {
+    "revision": "fc254d16be884e431332",
+    "url": "/react_portfolio_app/static/js/2.73c5cb79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio_app/static/js/2.73c5cb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd17a0ef35fe3684a5ae",
+    "url": "/react_portfolio_app/static/js/main.23e901e2.chunk.js"
+  },
+  {
+    "revision": "c0546710678e4ee1bf4b",
+    "url": "/react_portfolio_app/static/js/runtime-main.a7c4cd1e.js"
+  },
+  {
+    "revision": "eb585fb26a8f50d8bb7db0537cef2dc7",
+    "url": "/react_portfolio_app/static/media/about-icon.eb585fb2.png"
+  },
+  {
+    "revision": "7382cd75812286cf65d8d8caaed04c7c",
+    "url": "/react_portfolio_app/static/media/header-icon.7382cd75.png"
+  },
+  {
+    "revision": "e779f644e5212ecba2c7a9a9de58723c",
+    "url": "/react_portfolio_app/static/media/idea.e779f644.png"
+  },
+  {
+    "revision": "df725d203d80b6a72830066fa51d6646",
+    "url": "/react_portfolio_app/static/media/project-1.df725d20.PNG"
+  },
+  {
+    "revision": "0dd786137bf89cef208280567661d363",
+    "url": "/react_portfolio_app/static/media/project-2.0dd78613.PNG"
+  },
+  {
+    "revision": "ee93693fff083f3105bf54d1b53e53bc",
+    "url": "/react_portfolio_app/static/media/project-3.ee93693f.PNG"
+  },
+  {
+    "revision": "9bc10115c61326b96fd9b0d76ffb793a",
+    "url": "/react_portfolio_app/static/media/wave.9bc10115.svg"
+  },
+  {
+    "revision": "b03b2d6d77a30b0b1cd087e9344e8b1e",
+    "url": "/react_portfolio_app/static/media/wave2.b03b2d6d.svg"
+  }
+]);
